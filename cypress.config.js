311f8bc8e9343a33da1906f2224bd24e5626f617
Luan Silva <luan_henrique.s@hotmail.com>
2024-05-31 17:06:01 -0300

    add useful comment

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:8080',
+    baseUrl: 'http://localhost:8080', //it will not work in the pipeline right now, because to manage it to work we need to run the server locally. Thinking in a pipeline behavior in a real world we should setup a job with the proper configs to run the server before running the cypress tests. 
     env: {
       validAuthTokenAuthorised: 'eyJhbGciOiAibm9uZSIsInR5cCI6ICJKV1QifQ==.eyJzY29wZSI6ICJhY2NvdW50cyBjb25zZW50OnVybjpiYW5rOmU2NzNmNWRkLTk0MjYtNDFmMC1hZjE2LWUxMDc4OTA1NDVkMSIsImNsaWVudDEifQ==.',
       validAuthTokenRejected: 'eyJhbGciOiAibm9uZSIsInR5cCI6ICJKV1QifQ==.eyJzY29wZSI6ICJhY2NvdW50cyBjb25zZW50OnVybjpiYW5rOjdiNGQ0MThmLTlmNTAtNGRlNi04YjY5LTRmOGViYjAwYWY2NyIsImNsaWVudF9pZCI6ImNsaWVudDEifQ==.',
